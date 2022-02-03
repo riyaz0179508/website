6b2fb336e38512c373fa37c5823634707790ff4a
riyaz0179508 <riyazahmed2930@gmail.com>
2022-02-03 10:43:09 +0600

    3 Feb/22

--- lib/helper/custom_widget/custom_text_form_field.dart
+++ lib/helper/custom_widget/custom_text_form_field.dart
@@ -19,6 +19,8 @@ class CustomTextField extends StatefulWidget {
   _CustomTextFieldState createState() => _CustomTextFieldState();
 }
 
+String pass="";
+
 class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
@@ -27,10 +29,35 @@ class _CustomTextFieldState extends State<CustomTextField> {
         padding: const EdgeInsets.only(right: 20, left: 20),
         child: TextFormField(
           validator: (value){
+            bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                .hasMatch(value!);
             if(value == null || value.isEmpty){
               return "Field is Empty";
             }
+            if(widget.hintText=="Enter your Email"){
+              if(!emailValid){
+                return "Invalid email format";
+              }
+              if(widget.hintText=="Enter Password"){
+                pass= value;
+                if(value.length<6){
+                  return "Password must be atlast 6 Character";
+                }
+              }
 
+              if(widget.hintText=="Enter Password" || widget.hintText=="Conform Password"){
+                if(value.length>8){
+                  return "Password must be highest 8 Character";
+                }
+              }
+
+              if(widget.hintText=="Conform Password"){
+                if(pass != value){
+                  return "Password didn't match";
+                }
+              }
+
+            }
           },
           obscureText: widget.obsecureValue,
         controller: widget.emailController,
--- lib/screen/Sign_up_page.dart
+++ lib/screen/Sign_up_page.dart
@@ -1,3 +1,6 @@
+import 'package:beca_kena/helper/custom_widget/custom_button.dart';
+import 'package:beca_kena/helper/custom_widget/custom_text_form_field.dart';
+import 'package:beca_kena/helper/login_page/login_page.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -7,11 +10,64 @@ class SignUpPage extends StatefulWidget {
   _SignUpPageState createState() => _SignUpPageState();
 }
 
+final GlobalKey<FormState> _formKey =GlobalKey();
+TextEditingController _emailController=TextEditingController();
+TextEditingController _passController=TextEditingController();
+TextEditingController _conformPassController=TextEditingController();
+
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+       body: Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Icon(Icons.star, color: allColor.iconColor,),
+            SizedBox(height: 50,),
+            CustomTextField(
+              emailController: _emailController,
+              hintText: "Enter your Email",
+              obsecureValue: false,
+            ),
+
+            SizedBox(height: 20,),
+            CustomTextField(
+              emailController: _passController,
+              hintText: "Enter Password",
+              obsecureValue: true,
+
+            ),
+            CustomTextField(
+              emailController: _conformPassController,
+              hintText: "Conform Password",
+              obsecureValue: true,
+
+            ),
+            SizedBox(height: 20,),
+            InkWell(
+              onTap: (){
+                if(_formKey.currentState!.validate());
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+              },
+              child: CustomButton(
+                height: 50,
+                width: 300,
+                btnText: "Sign Up",
+              ),
+            ),
+
+            SizedBox(height: 20,),
+
+
+
+          ],
+        ),
+      ),
+    ),
+
     );
   }
 }
